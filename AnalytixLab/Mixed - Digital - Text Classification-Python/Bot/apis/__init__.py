7c6d87bd306cb48a4e5838031ab4dea56e190b3f
99sbr <y13uc295@lnmiit.ac.in>
2025-05-28 22:44:30 +0530

    commit

--- AnalytixLab/Mixed - Digital - Text Classification-Python/Bot/apis/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask_restplus import Api
-from flask import Blueprint
-
-blueprint =Blueprint ('api',__name__)
-from .namespace import api as ns1
-
-api = Api(blueprint)
-api.add_namespace(ns1)
--- AnalytixLab/Mixed - Digital - Text Classification-Python/Bot/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from .apis import blueprint as api
-from waitress import serve
-
-app = Flask('Restplus API')
-app.register_blueprint(api,url_prefix='/api/v1')
-app.config["SWAGGER_UI_JSONEDITOR"]=True
-app.run(port=1234,threaded=False)
-# serve(app,port=1234)
